bb494e326b1843b2bc5174ddd42611cc581737fa
Felipe Sabino <felipe@sabino.me>
2020-03-29 17:05:24 -0300

    nesting environments - chapter 8

--- src/com/sabino/klox/Environment.kt
+++ src/com/sabino/klox/Environment.kt
@@ -6,23 +6,39 @@ import java.util.*
 
 internal class Environment {
     private val values = mutableMapOf<String, Optional<Any>>()
+    private val enclosing: Optional<Environment>
+
+    constructor() { this.enclosing = Optional.empty() }
+    constructor(enclosing: Environment) { this.enclosing = Optional.of(enclosing) }
 
     fun define(name: String, value: Optional<Any>) {
         values[name] = value
     }
 
-    fun get(token: Token) : Optional<Any> {
-        return values.getOrElse(token.lexeme, {
-            throw Interpreter.RuntimeError(token, "Undefined variable '${token.lexeme}'.")
-        })
+    fun get(token: Token): Optional<Any> {
+
+        if (values.containsKey(token.lexeme)) {
+            return values.getOrElse(token.lexeme, { throw undefinedValiableError(token) })
+        }
+
+        if (enclosing.isPresent) { return enclosing.get().get(token) }
+
+        throw undefinedValiableError(token)
     }
 
-    fun assign(name: Token, value: Optional<Any>) {
+    fun assign(token: Token, value: Optional<Any>) {
 
-        if (values.containsKey(name.lexeme)) {
-            values[name.lexeme] = value
+        if (values.containsKey(token.lexeme)) {
+            values[token.lexeme] = value
+        } else if (enclosing.isPresent) {
+            enclosing.get().assign(token, value)
         } else {
-            throw RuntimeError(name, "Undefined variable '${name.lexeme}'.")
+            throw undefinedValiableError(token)
         }
+    }
+
+    @Throws
+    private fun undefinedValiableError(name: Token): RuntimeError {
+        return RuntimeError(name, "Undefined variable '${name.lexeme}'.")
     }
 }
